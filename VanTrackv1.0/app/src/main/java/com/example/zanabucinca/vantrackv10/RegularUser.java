d32f889fed18acad22ce75214105675b01287fe5
zaanab <zanabucinca@gmail.com>
2015-05-16 00:08:53 +0300

    --

--- VanTrackv1.0/app/src/main/java/com/example/zanabucinca/vantrackv10/RegularUser.java
+++
@@ -1,9 +0,0 @@
-package com.example.zanabucinca.vantrackv10;
-
-
-public class RegularUser extends User {
-    private static String mode="regular";
-    public String getMode(){
-        return mode;
-    }
-}
