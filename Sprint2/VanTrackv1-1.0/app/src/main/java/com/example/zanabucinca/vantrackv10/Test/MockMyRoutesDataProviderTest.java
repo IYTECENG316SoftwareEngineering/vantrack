b3c16a4d1042c8c2193b2bf198d625b7cf9abfc6
zaanab <zanabucinca@gmail.com>
2015-05-16 00:19:14 +0300

    --

--- Sprint2/VanTrackv1-1.0/app/src/main/java/com/example/zanabucinca/vantrackv10/Test/MockMyRoutesDataProviderTest.java
+++ Sprint2/VanTrackv1-1.0/app/src/main/java/com/example/zanabucinca/vantrackv10/Test/MockMyRoutesDataProviderTest.java
@@ -17,33 +17,31 @@ import java.util.HashMap;
 
 public class MockMyRoutesDataProviderTest extends AndroidTestCase {
 	public void testMockRoutesFromMyRoutes(){
-		//Step 1: Create data you want to return and put it into a matrix cursor
-		//In this case I am mocking getting phone numbers from Contacts Provider
+		
 		String[] exampleData = {"TEST-TEST"};
 		String[] examleProjection = new String[] { MyRoutesContract.ROUTE_NAME};
 		MatrixCursor matrixCursor = new MatrixCursor(examleProjection);
 		matrixCursor.addRow(exampleData);
 
-		//Step 2: Create a stub content provider and add the matrix cursor as the expected result of the query
+		
 		HashMapMockContentProvider mockProvider = new HashMapMockContentProvider();
 		mockProvider.addQueryResult(MyRoutesContract.CONTENT_URI, matrixCursor);
 
-		//Step 3: Create a mock resolver and add the content provider.
+		
 		MockContentResolver mockResolver = new MockContentResolver();
-		mockResolver.addProvider(MyRoutesContract.AUTHORITY /*Needs to be the same as the authority of the provider you are mocking */, mockProvider);
+		mockResolver.addProvider(MyRoutesContract.AUTHORITY, mockProvider);
 
-		//Step 4: Add the mock resolver to the mock context
+		
 		ContextWithMockContentResolver mockContext = new ContextWithMockContentResolver(super.getContext());
 		mockContext.setContentResolver(mockResolver);
 
-		//Example Test
+		
 		ExampleClassUnderTest underTest = new ExampleClassUnderTest();
 		String result = underTest.getRoute(mockContext);
 		assertEquals("TEST-TEST",result);
 	}
 
-	//Specialized Mock Content provider for step 2.  Uses a hashmap to return data dependent on the uri in the query
-	public class HashMapMockContentProvider extends MockContentProvider {
+		public class HashMapMockContentProvider extends MockContentProvider {
 		private HashMap<Uri, Cursor> expectedResults = new HashMap<Uri, Cursor>();
 		public void addQueryResult(Uri uriIn, Cursor expectedResult){
 			expectedResults.put(uriIn, expectedResult);
@@ -59,12 +57,12 @@ public class MockMyRoutesDataProviderTest extends AndroidTestCase {
 		public void setContentResolver(ContentResolver contentResolver){ this.contentResolver = contentResolver;}
 		public ContextWithMockContentResolver(Context targetContext) { super(targetContext, "test");}
 		@Override public ContentResolver getContentResolver() { return contentResolver; }
-		@Override public Context getApplicationContext(){ return this; } //Added in-case my class called getApplicationContext()
+		@Override public Context getApplicationContext(){ return this; }
 	}
 
-	//An example class under test which queries the populated cursor to get the expected phone number
+	
 	public class ExampleClassUnderTest{
-		public  String getRoute(Context context){//Query for  phone numbers from contacts
+		public  String getRoute(Context context){
 			String[] projection = new String[]{ MyRoutesContract.ROUTE_NAME};
 			Cursor cursor= context.getContentResolver().query(MyRoutesContract.CONTENT_URI, projection, null, null, null);
 			cursor.moveToNext();
